f000fae4245d90fe68982b7b7cda6db8c432ab35
承翼 <chengyi.lxg@alibaba-inc.com>
2025-12-02 21:05:56 +0800

    update client

--- internal/agentbay/client.go
+++ internal/agentbay/client.go
@@ -1263,7 +1263,9 @@ type XMLGetDockerfileTemplateResponse struct {
 	RequestId      string   `xml:"RequestId"`
 	HttpStatusCode int      `xml:"HttpStatusCode"`
 	Data           struct {
-		OssDownloadUrl string `xml:"OssDownloadUrl"`
+		OssDownloadUrl    string `xml:"OssDownloadUrl"`
+		NonEditLineNum    int32  `xml:"NonEditLineNum"`
+		DockerfileContent string `xml:"DockerfileContent"`
 	} `xml:"Data"`
 	Code    string `xml:"Code"`
 	Success bool   `xml:"Success"`
@@ -1287,6 +1289,8 @@ func (cw *clientWrapper) parseGetDockerfileTemplateXMLResponse(xmlData []byte) (
 	log.Debugf("[DEBUG] - Success: %t", xmlResp.Success)
 	log.Debugf("[DEBUG] - Message: %s", xmlResp.Message)
 	log.Debugf("[DEBUG] - OssDownloadUrl: %s", xmlResp.Data.OssDownloadUrl)
+	log.Debugf("[DEBUG] - NonEditLineNum: %d", xmlResp.Data.NonEditLineNum)
+	log.Debugf("[DEBUG] - DockerfileContent length: %d", len(xmlResp.Data.DockerfileContent))
 
 	// Convert to SDK response format
 	response := &client.GetDockerfileTemplateResponse{
@@ -1299,12 +1303,14 @@ func (cw *clientWrapper) parseGetDockerfileTemplateXMLResponse(xmlData []byte) (
 			Success:        dara.Bool(xmlResp.Success),
 			Message:        dara.String(xmlResp.Message),
 			Data: &client.GetDockerfileTemplateResponseBodyData{
-				OssDownloadUrl: dara.String(xmlResp.Data.OssDownloadUrl),
+				OssDownloadUrl:    dara.String(xmlResp.Data.OssDownloadUrl),
+				NonEditLineNum:    dara.Int32(xmlResp.Data.NonEditLineNum),
+				DockerfileContent: dara.String(xmlResp.Data.DockerfileContent),
 			},
 		},
 	}
 
-	log.Debugf("[DEBUG] Created GetDockerfileTemplate SDK response with OssDownloadUrl: %s", xmlResp.Data.OssDownloadUrl)
+	log.Debugf("[DEBUG] Created GetDockerfileTemplate SDK response with OssDownloadUrl: %s, NonEditLineNum: %d", xmlResp.Data.OssDownloadUrl, xmlResp.Data.NonEditLineNum)
 	return response, nil
 }
 
@@ -1332,6 +1338,9 @@ func (cw *clientWrapper) GetDockerfileTemplate(ctx context.Context, request *cli
 	query := map[string]interface{}{}
 	if !dara.IsNil(request.Source) {
 		query["Source"] = request.Source
+	}
+	if !dara.IsNil(request.SourceImageId) {
+		query["SourceImageId"] = request.SourceImageId
 	}
 	// Template is reserved for future extension
 	if !dara.IsNil(request.Template) {
